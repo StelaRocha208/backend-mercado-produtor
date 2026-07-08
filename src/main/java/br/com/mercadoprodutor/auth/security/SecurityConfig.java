4e23606c25b6b2f1ae0502a11bf18a885352cb3d
Glauber <20232ireads0016@ifba.edu.br>
2026-07-08 15:59:38 -0300

    feat(MDP-28) Implementa componente do mapa de ocupação

--- src/main/java/br/com/mercadoprodutor/auth/security/SecurityConfig.java
+++ src/main/java/br/com/mercadoprodutor/auth/security/SecurityConfig.java
@@ -61,6 +61,14 @@ public class SecurityConfig {
                         
 
 
+                        // Liberação temporária para testes do mapa
+                        .requestMatchers(HttpMethod.GET, "/api/secoes").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/espacos").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/espacos/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/ocupacao/**").permitAll()
+                        
+
+
                         // Rotas de configuração para redefinição de senha
                         .requestMatchers(HttpMethod.POST, "/auth/esqueci-senha").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/redefinir-senha").permitAll()
